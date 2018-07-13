020438977018d6aaa25c3f8318e7c0235c8556f4
rachel1032 <racheles32@gmail.com>
2018-07-13 15:26:13 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,13 +1,15 @@
 # dog.rb
 
 class Dog
-end
+
   
   def initialize(arg1)
     @bark = arg1
     puts "Woof!"
   end
   
+end 
+
   def initialize(arg2)
     @sit = arg2
     puts "The Dog is sitting"
