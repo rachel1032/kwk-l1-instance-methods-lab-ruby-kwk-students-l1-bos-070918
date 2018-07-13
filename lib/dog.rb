1df4e57d53065f3c385bb845da5eaebb5ddde370
rachel1032 <racheles32@gmail.com>
2018-07-13 15:27:24 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -2,17 +2,13 @@
 
 class Dog
   attr_reader
-attr_accessor
+  attr_accessor
   
-  def initialize(arg1)
+  def initialize(arg1, arg2)
     @bark = arg1
-    @sit 
+    @sit = arg2
     puts "Woof!"
+    puts "The Dog is sitting"
   end
   
 end 
-
-  def initialize(arg2)
-    @sit = arg2
-    puts "The Dog is sitting"
-  end
