26464f6446b2168b10aef7f6f6f5ac35ac0064f0
rachel1032 <racheles32@gmail.com>
2018-07-13 15:23:09 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -3,10 +3,12 @@
 class Dog
 end
   
-  def Dog(#bark)
+  def Dog(bark)
+    @bark = bark
     puts "Woof!"
   end
   
-  def Dog(#sit)
+  def Dog(sit)
+    @sit = sit
     puts "The Dog is sitting"
   end
